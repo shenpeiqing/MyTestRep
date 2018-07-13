6ac904ae1a4dc2292a47797f1410db594b10a956
master <284309510@qq.com>
2018-07-13 21:11:19 +0800

    十分士大夫

--- untitled/src/org/westos/demo/JavaClass.java
+++ untitled/src/org/westos/demo/JavaClass.java
@@ -2,6 +2,6 @@ package org.westos.demo;
 
 public class JavaClass {
     public static void main(String[] args) {
-        System.out.println("hehehehhehehe");
+        System.out.println("hehehehhehehe");阿斯顿发射点发射点
     }
 }
